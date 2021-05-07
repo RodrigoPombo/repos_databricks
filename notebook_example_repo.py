c235a1ee7623ae51fa9b3156d2a8c97c244c0275
rodes2.pombo@gmail.com <rodes2.pombo@gmail.com>
2021-05-07 16:54:07 +0000

    last
    last

--- notebook_example_repo.py
+++ notebook_example_repo.py
@@ -1,14 +1,3 @@
 # Databricks notebook source
 # MAGIC %python
 # MAGIC print("zzzzzzzzzzzzzzzzzzzzz")
-
-# COMMAND ----------
-
-# MAGIC %python
-# MAGIC print("vnet")
-
-# COMMAND ----------
-
-# MAGIC %python
-# MAGIC 
-# MAGIC print("xxxxxxxxx")
